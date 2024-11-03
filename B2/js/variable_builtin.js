5f955b03f2d27a38295572647065483ba9136f9a
Nhan10092008 <Tranngocnhan100908@gmail.com>
2024-11-03 11:37:06 +0700

    Buổi 3

---
+++ B2/js/variable_builtin.js
@@ -0,0 +1,26 @@
+// input, output ---
+// alert("This is notification!");
+// const full_name = prompt("Nhap ten cua ban");
+// console.log(typeof full_name);
+// const is_human = confirm("Are you human?");
+// console.log(is_human);
+// variable (scope) ------------------------
+// *** var
+// for (var index = 0; index < 10; index++) {
+//     console.log(index);
+//   }
+  
+//   console.log(index); // 10
+  
+  // ***let
+//   for (let index = 0; index < 10; index++) {
+//     console.log(index);
+//   }
+  
+//   console.log(index); // error (is not defined)
+ 
+//   // *** const
+// const object = { name: "abc", age: 15 };
+// // object -= {} // error (is constant var)
+// object.name = "xyz";
+// console.table(object);
